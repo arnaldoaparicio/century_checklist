0d700334a1f18f6fe29bdeb0f7971db409780414
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-25 10:29:32 -0400

    Add test to check for greeting on welcome page

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,2 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
+RSpec.describe "Welcome Page", type: :feature do
+  it "shows greeting" do
+    visit("/")
+
+    expect(page).to have_content("Welcome! Please make sure to check everything!")
+  end
+end
