e4c9418f8b75808cd22360f8c9f5a3d7870b7a0d
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-05-02 16:47:25 -0400

    Add test to check for link

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "Welcome Page", type: :feature do
 
     expect(page).to have_content("Welcome! Please make sure to check everything!")
   end
+
+  it 'shows a link to lists' do
+    visit('/')
+
+    expect(page).to have_link('/lists')
+  end
 end
