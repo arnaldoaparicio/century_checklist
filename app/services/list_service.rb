b19389a336fb9065ee96b098869f39e8913b451d
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-05-03 17:15:50 -0400

    Add 'connection' method

--- app/services/list_service.rb
+++ app/services/list_service.rb
@@ -1,8 +1,10 @@
 class ListService
-
   def self.all_created_lists
     response = connection.get('/api/v1/lists')
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.connection
+    Faraday.new(url: 'http://localhost:3000')
+  end
 end
