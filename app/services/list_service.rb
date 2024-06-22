8266d2bfce71b412172c77db5f4fc727314b02d7
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-21 20:54:25 -0400

    Add 'single_list' method

--- app/services/list_service.rb
+++ app/services/list_service.rb
@@ -4,6 +4,11 @@ class ListService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.single_list(list_id)
+    response = connection.get("/api/v1/lists/#{list_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.connection
     Faraday.new(url: 'http://localhost:3000')
   end
