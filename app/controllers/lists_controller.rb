7980ce79d1afce8672054dd86700de37665bcfea
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-08-11 16:58:35 -0400

    Add 'lists_items' method from List facade

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,8 +3,10 @@ class ListsController < ApplicationController
   def index
     @lists = ListFacade.all_lists
   end
-
+  
   def show 
     @list = ListFacade.single_list(params[:id])
+    @lists_items = ListFacade.lists_items(params[:id])
+    require "pry"; binding.pry
   end
 end
